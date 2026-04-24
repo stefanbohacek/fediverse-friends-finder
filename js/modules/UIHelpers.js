721cddc6e77056672bd1008d05f4eb23280dd28f
Stefan Bohacek <stefan@stefanbohacek.com>
2026-04-24 09:24:14 -0400

    Code cleanup.

--- js/modules/UIHelpers.js
+++ js/modules/UIHelpers.js
@@ -3,13 +3,13 @@ export const setLoadingStatus = (message) => {
 };
 
 export const setError = (message) => {
-  const alert = document.getElementById("error-alert");
+  const alertEl = document.getElementById("error-alert");
   if (message) {
-    alert.textContent = message.includes("Invalid handle")
+    alertEl.textContent = message.includes("Invalid handle")
       ? "Please make sure to use your full username, for example: username.bsky.app."
       : message;
-    alert.classList.remove("d-none");
+    alertEl.classList.remove("d-none");
   } else {
-    alert.classList.add("d-none");
+    alertEl.classList.add("d-none");
   }
 };
