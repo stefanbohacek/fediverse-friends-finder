db5e059700ddd8ac1461349cfd4e10777a4af030
Stefan Bohacek <stefan@stefanbohacek.com>
2026-04-24 20:24:39 -0400

    Updated example username.

--- js/modules/UIHelpers.js
+++ js/modules/UIHelpers.js
@@ -6,7 +6,7 @@ export const setError = (message) => {
   const alertEl = document.getElementById("error-alert");
   if (message) {
     alertEl.textContent = message.includes("Invalid handle")
-      ? "Please make sure to use your full username, for example: username.bsky.app."
+      ? "Please make sure to use your full username, for example: username.bsky.social."
       : message;
     alertEl.classList.remove("d-none");
   } else {
